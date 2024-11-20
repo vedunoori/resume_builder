eb995dfe62c3f78cd613d84cc238f674eacba192
sridhar <vedunoori.sreedhar@gmail.com>
2024-11-20 14:17:55 +0530

    new resume builder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Helmet } from 'react-helmet';
 import './App.css';
+import ResumeState from './Context/ResumeState';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
+import About from './Pages/About/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ResumeState>
+      <div className="App">
+        <Helmet>
+          <title>Resume Builder - Create Professional Resumes Online</title>
+          <meta name="description" content="Build and customize professional resumes online with Resume Builder. Choose from a variety of templates and create your perfect resume easily." />
+          <meta name="keywords" content="resume builder, professional resumes, online resumes, resume templates" />
+          <meta name="author" content="Hardik Desai" />
+          <meta property="og:title" content="Resume Builder - Create Professional Resumes Online" />
+          <meta property="og:description" content="Build and customize professional resumes online with Resume Builder. Choose from a variety of templates and create your perfect resume easily." />
+          <meta property="og:image" content="https://avatars.githubusercontent.com/u/87645745?v=4" />
+          <meta property="og:url" content="https://quick-resume.netlify.app/" />
+          <meta property="og:type" content="website" />
+        </Helmet>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </ResumeState>
   );
 }
 
